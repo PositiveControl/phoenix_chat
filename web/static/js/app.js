34489d6d7220e9920d663411a75085beadd91b1a
Mark Evans <mark.evans@teamsnap.com>
2015-08-07 18:40:24 -0700

    phx chat tutorial w/PR solution comments

---
+++ web/static/js/app.js
@@ -0,0 +1,52 @@
+// Brunch automatically concatenates all files in your
+// watched paths. Those paths can be configured at
+// config.paths.watched in "brunch-config.js".
+//
+// However, those files will only be executed if
+// explicitly imported. The only exception are files
+// in vendor, which are never wrapped in imports and
+// therefore are always executed.
+
+// Import dependencies
+//
+// If you no longer want to use a dependency, remember
+// to also remove its path from "config.paths.watched".
+import "deps/phoenix_html/web/static/js/phoenix_html"
+// Import local files
+//
+// Local files can be imported directly using relative
+// paths "./socket" or full ones "web/static/js/socket".
+
+
+// solution two - comment out
+import {Socket} from "deps/phoenix/web/static/js/phoenix"
+// end solution
+
+// solution two - uncomment
+// import socket from "./socket"
+// end solution
+
+let chatInput = $("#chat-input")
+let messagesContainer = $("#messages")
+
+// solution two - comment out
+let socket = new Socket("/socket")
+// end solution
+
+socket.connect()
+let chan = socket.channel("rooms:lobby", {})
+
+chatInput.on("keypress", event => {
+  if(event.keyCode === 13){
+    chan.push("new_msg", {body: chatInput.val()})
+    chatInput.val("")
+  }
+})
+
+chan.on("new_msg", payload => {
+  messagesContainer.append(`<br/>[${Date()}] ${payload.body}`)
+})
+
+chan.join().receive("ok", chan => {
+  console.log("Welcome to ChatterBox")
+})
